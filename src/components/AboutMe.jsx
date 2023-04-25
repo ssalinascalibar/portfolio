01bee698fe18d4e45f8426f37dc35589bd599eab
ssalinascalibar <ssalinascalibar@gmail.com>
2023-04-24 22:54:01 -0400

    add animated css aos to components

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,7 +6,7 @@ export default function AboutMe() {
       <h2>Acerca de mí</h2>
       <div className="row">
         <div className="col">
-          <p className="p-section">
+          <p className="p-section" data-aos="fade-left" data-aos-duration="3000">
             "Mediante la carrera de Comunicación Audiovisual y Multimedia conocí
             el diseño web de sitios estáticos, esto impulsó para posteriormente
             profundizar e incursionar en el desarrollo web"
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,9 @@ export default function Header() {
             <div className="row">
                 <div className="col">
                     <div className="header-text">
-                        <p>Hola, soy</p>
-                        <h1><span className="span-header">Sebastian Salinas.</span><br/>Te invito a visitar mi portfolio.</h1>
-                        <p>Aquí podrás encontrar parte de mis trabajos y proyectos realizados en el área del desarrollo Web.</p>
+                        <p data-aos="fade-right" data-aos-duration="3000">Hola, soy</p>
+                        <h1 data-aos="fade-left" data-aos-duration="3000"><span className="span-header">Sebastian Salinas.</span><br/>Te invito a visitar mi portfolio.</h1>
+                        <p data-aos="fade-right" data-aos-duration="3000">Aquí podrás encontrar parte de mis trabajos y proyectos realizados en el área del desarrollo Web.</p>
                     </div>
                 </div>
             </div>
