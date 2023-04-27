a7c324aa6cff077d6fb016a617aa748908b9a93b
ssalinascalibar <ssalinascalibar@gmail.com>
2023-04-27 19:57:42 -0400

    mod animation aos

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,9 +10,9 @@ export default function Header() {
             <div className="row">
                 <div className="col">
                     <div className="header-text">
-                        <p data-aos="fade-right" data-aos-duration="3000">Hola, soy</p>
-                        <h1 data-aos="fade-left" data-aos-duration="3000"><span className="span-header">Sebastian Salinas.</span><br/>Te invito a visitar mi portfolio.</h1>
-                        <p data-aos="fade-right" data-aos-duration="3000">Aquí podrás encontrar parte de mis trabajos y proyectos realizados en el área del desarrollo Web.</p>
+                        <p data-aos="fade-down" data-aos-duration="1000">Hola, soy</p>
+                        <h1 data-aos="fade" data-aos-duration="3000"><span className="span-header">Sebastian Salinas.</span><br/>Te invito a visitar mi portfolio.</h1>
+                        <p data-aos="fade" data-aos-duration="3000">Aquí podrás encontrar parte de mis trabajos y proyectos realizados en el área del desarrollo Web.</p>
                     </div>
                 </div>
             </div>
