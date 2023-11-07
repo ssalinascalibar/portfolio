1721092e1fda3b03de477ac2fc3c7b3c0dd81906
ssalinascalibar <ssalinascalibar@gmail.com>
2023-11-07 12:14:01 -0300

    add text to header description

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,8 @@ export default function Header() {
             <div className="row">
                 <div className="col">
                     <div className="header-text">
-                        <p data-aos="fade-down" data-aos-duration="1000">Hola, soy</p>
-                        <h1 data-aos="fade" data-aos-duration="3000"><span className="span-header">Sebastian Salinas.</span><br/>Te invito a visitar mi portafolio.</h1>
+                        <p data-aos="fade-down" data-aos-duration="1000">Hola, mi nombre es</p>
+                        <h1 data-aos="fade" data-aos-duration="3000"><span className="span-header">Sebastian Salinas.</span><br/>Soy desarrollador Front End y te invito a visitar mi portafolio.</h1>
                         <p data-aos="fade" data-aos-duration="3000">Aquí podrás encontrar parte de mis trabajos y proyectos realizados en el área del desarrollo Web.</p>
                     </div>
                 </div>
