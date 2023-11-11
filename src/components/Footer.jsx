ea3dbce3906ff4f9ea038b3913000c708174c591
ssalinascalibar <ssalinascalibar@gmail.com>
2023-11-10 23:38:51 -0300

    add rel to a link footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,9 +11,9 @@ export default function Footer() {
   return (
     <footer>
       <div className="contact">
-        <a href="https://www.linkedin.com/in/sebastian-salinas-calibar-9b070a23a/" target="_blank"><i className="fa-brands fa-linkedin"></i></a>
-        <a href="https://github.com/ssalinascalibar" target="_blank"><i className="fa-brands fa-github"></i></a>
-        <a href="mailto:sf.salinascalibar@gmail.com" target="_blank"><i className="fa-solid fa-envelope"></i></a>
+        <a href="https://www.linkedin.com/in/sebastian-salinas-calibar-9b070a23a/" target="_blank" rel="nofollow noopener noreferrer"><i className="fa-brands fa-linkedin"></i></a>
+        <a href="https://github.com/ssalinascalibar" target="_blank" rel="nofollow noopener noreferrer"><i className="fa-brands fa-github"></i></a>
+        <a href="mailto:sf.salinascalibar@gmail.com" target="_blank" rel="nofollow noopener noreferrer"><i className="fa-solid fa-envelope"></i></a>
       </div>
       <p className="text-center">&copy; {year} Sebastian Salinas Calibar</p>
     </footer>
