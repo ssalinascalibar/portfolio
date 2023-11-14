d281323a9527260a432791f12f206d1ccbccc9a3
ssalinascalibar <ssalinascalibar@gmail.com>
2023-11-14 18:21:19 -0300

    mod padding header text

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,31 @@
-import React from 'react'
+import React from "react";
 
 //css
-import '../assets/css/header.css'
+import "../assets/css/header.css";
 
 export default function Header() {
   return (
     <header>
-        <div className="container">
-            <div className="row">
-                <div className="col">
-                    <div className="header-text">
-                        <p data-aos="fade-down" data-aos-duration="1000">Hola, mi nombre es</p>
-                        <h1 data-aos="fade" data-aos-duration="3000"><span className="span-header">Sebastian Salinas.</span><br/>Soy desarrollador Front End y te invito a visitar mi portafolio.</h1>
-                        <p data-aos="fade" data-aos-duration="3000">Aquí podrás encontrar parte de mis trabajos y proyectos realizados en el área del desarrollo Web.</p>
-                    </div>
-                </div>
+      <div className="container">
+        <div className="row">
+          <div className="col">
+            <div className="header-text">
+              <p data-aos="fade-down" data-aos-duration="1000">
+                Hola, mi nombre es
+              </p>
+              <h1 data-aos="fade" data-aos-duration="3000">
+                <span className="span-header">Sebastian Salinas.</span>
+                <br />
+                Soy desarrollador Front End y te invito a visitar mi portafolio.
+              </h1>
+              <p data-aos="fade" data-aos-duration="3000">
+                Aquí podrás encontrar parte de mis trabajos y proyectos
+                realizados en el área del desarrollo Web.
+              </p>
             </div>
+          </div>
         </div>
+      </div>
     </header>
-  )
+  );
 }
